4ff9d0d97914d1243f2adbeee110c06e7d695703
Angie Santacruz <127572835+AngieSant@users.noreply.github.com>
2025-04-28 23:40:14 -0500

    Update productos.js
    data

--- js/productos.js
+++ js/productos.js
@@ -1,1 +1,456 @@
+const zapatos = [
+  
+    {
+      nombre: "Deportivo Ligero",
+      precio: 79990,
+      talla: 40,
+      codigo: "aB92Cd12",
+      imagen: "/img/img1.jpg",
+      categoria: "Deportivo",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Casual Confort",
+      precio: 59990,
+      talla: 37,
+      codigo: "xY27Zp89",
+      imagen: "/img/img2.jpg",
+      categoria: "Casual",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Sandalia Verano",
+      precio: 39500,
+      talla: 38,
+      codigo: "pQ12Rs56",
+      imagen: "/img/img3.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Formal Clásico",
+      precio: 99000,
+      talla: 42,
+      codigo: "mN89Op34",
+      imagen: "/img/img4.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Bota Invierno",
+      precio: 119990,
+      talla: 39,
+      codigo: "gH56Ij78",
+      imagen: "/img/img5.jpg",
+      categoria: "Botas",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Deportivo Urbano",
+      precio: 85000,
+      talla: 43,
+      codigo: "uV34Wx67",
+      imagen: "/img/img6.jpg",
+      categoria: "Deportivo",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Sandalia Playa",
+      precio: 29990,
+      talla: 36,
+      codigo: "eF21Gh43",
+      imagen: "/img/img7.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Mocasín Elegante",
+      precio: 75000,
+      talla: 41,
+      codigo: "rS78Tu90",
+      imagen: "/img/img8.jpg",
+      categoria: "Casual",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Zapato Tacón",
+      precio: 125500,
+      talla: 37,
+      codigo: "lK45Mn67",
+      imagen: "/img/img9.jpg",
+      categoria: "Formal",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Tenis Clásicos",
+      precio: 65000,
+      talla: 40,
+      codigo: "dB67Ef89",
+      imagen: "/img/img10.jpg",
+      categoria: "Casual",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Running Veloz",
+      precio: 95000,
+      talla: 41,
+      codigo: "cA34Fg56",
+      imagen: "/img/img1.jpg",
+      categoria: "Deportivo",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Balerina Cómoda",
+      precio: 49990,
+      talla: 38,
+      codigo: "wQ90Rt23",
+      imagen: "/img/img2.jpg",
+      categoria: "Casual",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Chancla Piscina",
+      precio: 25000,
+      talla: 39,
+      codigo: "oP67Zs12",
+      imagen: "/img/img3.jpg",
+      categoria: "Sandalias",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Oxford Moderno",
+      precio: 105000,
+      talla: 43,
+      codigo: "iL23Mk89",
+      imagen: "/img/img4.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Botín Casual",
+      precio: 89990,
+      talla: 36,
+      codigo: "yH89Ju45",
+      imagen: "/img/img5.jpg",
+      categoria: "Botas",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Entrenamiento Pro",
+      precio: 110000,
+      talla: 42,
+      codigo: "sV56Wn78",
+      imagen: "/img/img6.jpg",
+      categoria: "Deportivo",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Alpargata Verano",
+      precio: 45500,
+      talla: 37,
+      codigo: "dN12Er34",
+      imagen: "/img/img7.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Náutico Clásico",
+      precio: 80000,
+      talla: 40,
+      codigo: "fG78Ip90",
+      imagen: "/img/img8.jpg",
+      categoria: "Casual",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Stiletto Elegante",
+      precio: 135000,
+      talla: 38,
+      codigo: "bK34Lm56",
+      imagen: "/img/img9.jpg",
+      categoria: "Formal",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Zapatilla Urbana",
+      precio: 70000,
+      talla: 41,
+      codigo: "zC56Xy78",
+      imagen: "/img/img10.jpg",
+      categoria: "Casual",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Deportivo Transpirable",
+      precio: 88000,
+      talla: 39,
+      codigo: "qB23Cd45",
+      imagen: "/img/img1.jpg",
+      categoria: "Deportivo",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Manoletina Chic",
+      precio: 55000,
+      talla: 36,
+      codigo: "tY67Zp12",
+      imagen: "/img/img2.jpg",
+      categoria: "Casual",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Sandalia Plataforma",
+      precio: 42000,
+      talla: 40,
+      codigo: "rP89Qs34",
+      imagen: "/img/img3.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Derby Sofisticado",
+      precio: 115000,
+      talla: 44,
+      codigo: "jL12Mk78",
+      imagen: "/img/img4.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Bota de Montaña",
+      precio: 129990,
+      talla: 37,
+      codigo: "hH45Ju90",
+      imagen: "/img/img5.jpg",
+      categoria: "Botas",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Training Ligero",
+      precio: 92000,
+      talla: 40,
+      codigo: "wV78Wn34",
+      imagen: "/img/img6.jpg",
+      categoria: "Deportivo",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Cangrejera Moderna",
+      precio: 35500,
+      talla: 38,
+      codigo: "sN34Er56",
+      imagen: "/img/img7.jpg",
+      categoria: "Sandalias",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Blucher Casual",
+      precio: 78000,
+      talla: 42,
+      codigo: "kG90Ip12",
+      imagen: "/img/img8.jpg",
+      categoria: "Casual",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Zapato Salón",
+      precio: 140000,
+      talla: 39,
+      codigo: "mK23Lm45",
+      imagen: "/img/img9.jpg",
+      categoria: "Formal",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Sneaker Retro",
+      precio: 68000,
+      talla: 43,
+      codigo: "vC45Xy67",
+      imagen: "/img/img10.jpg",
+      categoria: "Casual",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Deportivo de Calle",
+      precio: 82000,
+      talla: 37,
+      codigo: "pB56Cd78",
+      imagen: "/img/img1.jpg",
+      categoria: "Deportivo",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Mercedita Cómoda",
+      precio: 52000,
+      talla: 41,
+      codigo: "uY89Zp23",
+      imagen: "/img/img2.jpg",
+      categoria: "Casual",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Sandalia Deportiva",
+      precio: 38000,
+      talla: 36,
+      codigo: "nP12Qs45",
+      imagen: "/img/img3.jpg",
+      categoria: "Sandalias",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Zapato Vestir",
+      precio: 108000,
+      talla: 40,
+      codigo: "oL34Mk90",
+      imagen: "/img/img4.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Bota de Trabajo",
+      precio: 135000,
+      talla: 42,
+      codigo: "tH67Ju12",
+      imagen: "/img/img5.jpg",
+      categoria: "Botas",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Atletismo Ligero",
+      precio: 98000,
+      talla: 38,
+      codigo: "rV90Wn45",
+      imagen: "/img/img6.jpg",
+      categoria: "Deportivo",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Hawaiana Casual",
+      precio: 22000,
+      talla: 44,
+      codigo: "qN23Er67",
+      imagen: "/img/img7.jpg",
+      categoria: "Sandalias",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Oxford Clásico",
+      precio: 95000,
+      talla: 39,
+      codigo: "sG56Ip23",
+      imagen: "/img/img8.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Plataforma Elegante",
+      precio: 150000,
+      talla: 35,
+      codigo: "nK45Lm78",
+      imagen: "/img/img9.jpg",
+      categoria: "Formal",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Zapatilla Deportiva",
+      precio: 72000,
+      talla: 44,
+      codigo: "wC78Xy90",
+      imagen: "/img/img10.jpg",
+      categoria: "Deportivo",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Deportivo de Moda",
+      precio: 89990,
+      talla: 36,
+      codigo: "aC12Fd34",
+      imagen: "/img/img1.jpg",
+      categoria: "Deportivo",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Slip-on Cómodo",
+      precio: 48000,
+      talla: 42,
+      codigo: "xR34Zt56",
+      imagen: "/img/img2.jpg",
+      categoria: "Casual",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Sandalia de Cuña",
+      precio: 41500,
+      talla: 37,
+      codigo: "pL56Rs78",
+      imagen: "/img/img3.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Zapato Inglés",
+      precio: 102000,
+      talla: 41,
+      codigo: "mO78Op12",
+      imagen: "/img/img4.jpg",
+      categoria: "Formal",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Bota de Lluvia",
+      precio: 79000,
+      talla: 40,
+      codigo: "gI90Ij34",
+      imagen: "/img/img5.jpg",
+      categoria: "Botas",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Cross Training",
+      precio: 105000,
+      talla: 44,
+      codigo: "uW23Wx56",
+      imagen: "/img/img6.jpg",
+      categoria: "Deportivo",
+      genero: "Unisex"
+    },
+    {
+      nombre: "Sandalia Casual",
+      precio: 32000,
+      talla: 39,
+      codigo: "eG45Gh78",
+      imagen: "/img/img7.jpg",
+      categoria: "Sandalias",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Mocasín Casual",
+      precio: 68000,
+      talla: 43,
+      codigo: "rT67Tu90",
+      imagen: "/img/img8.jpg",
+      categoria: "Casual",
+      genero: "Masculino"
+    },
+    {
+      nombre: "Zapato de Noche",
+      precio: 160000,
+      talla: 36,
+      codigo: "lM89Mn12",
+      imagen: "/img/img9.jpg",
+      categoria: "Formal",
+      genero: "Femenino"
+    },
+    {
+      nombre: "Zapatilla Urbana",
+      precio: 60000,
+      talla: 38,
+      codigo: "dC23Ef45",
+      imagen: "/img/img10.jpg",
+      categoria: "Casual",
+      genero: "Unisex"
+    }
+  
+];
 
+console.log(zapatos);
